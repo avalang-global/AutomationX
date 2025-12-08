2c55796fc8a2b7144a0278cb1347a8bc5421b6a6
Rupal <rupalbarman@gmail.com>
2025-12-08 16:43:19 +0700

    feat: speech to text support and cleanup deps

--- packages/widgets/vite.config.ts
+++ packages/widgets/vite.config.ts
@@ -4,7 +4,6 @@ import dts from 'vite-plugin-dts';
 import * as path from 'path';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
-import replace from '@rollup/plugin-replace';
 
 export default defineConfig(() => ({
   root: __dirname,
@@ -16,10 +15,6 @@ export default defineConfig(() => ({
     dts({
       entryRoot: 'src',
       tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
-    }),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-      preventAssignment: true,
     }),
   ],
   build: {
@@ -46,6 +41,10 @@ export default defineConfig(() => ({
           showdown: 'showdown',
         },
       },
+    },
+    define: {
+      // Required by React
+      'process.env.NODE_ENV': JSON.stringify('production'),
     },
   },
 }));
